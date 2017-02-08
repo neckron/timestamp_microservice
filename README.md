a28f8073b6f021660fe6197140714c6442ff668b
Francisco Rodriguez <francisco.rodriguez@prodigious.com>
2017-02-08 16:32:28 -0500

    User stories and README.md update

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # timestamp_microservice
 
-User Story: I can pass a string as a parameter, and it will check to see whether that string contains either a unix timestamp or a natural language date (example: January 1, 2016).
-User Story: If it does, it returns both the Unix timestamp and the natural language form of that date.
-User Story: If it does not contain a date or Unix timestamp, it returns null for those properties.
+ * **User Story:** I can pass a string as a parameter, and it will check to see whether that string contains either a unix timestamp or a natural language date (example: January 1, 2016).
+ * **User Story:** If it does, it returns both the Unix timestamp and the natural language form of that date.
+ * **User Story:** If it does not contain a date or Unix timestamp, it returns null for those properties.
 
+### Installing and Running
+
+To installing it on local dev env:
+
+```
+* install npm install
+* run node server.js
+```
+
+it'll run on port 8080.
+
+## Built With
+
+* [Node.js](https://nodejs.org/) - Execution environment
+* [Moment.js](https://momentjs.com) - Timestamp javascript library
+* [Express.js](http://expressjs.com/) - Web framework for Node.js programs
+* [ejs](http://www.embeddedjs.com/) - Templating engine
+
+## Authors
+
+* **Neckron**
--- server.js
+++ server.js
@@ -1,26 +1,37 @@
 var express = require('express');
-var moment = require('moment'); 
+var moment = require('moment');
 var app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  //id = req.body.id;
   res.render('index.ejs');
 })
 
 app.get('/:param', function(req, res) {
-    var valid = moment(req.params.param).isValid();
-    if(valid){
-        res.send({'unix':req.params.param,'natural':moment(req.params.param).toDate()})
-    }else{
-        res.send({'unix':'null','natural':'null'})
-    }
-  
+      if(isNumeric(req.params.param)){
+        var dateNatural = moment.unix(req.params.param).format("MMMM DD ,  YYYY");
+        if(moment(dateNatural).isValid()){
+          res.send({'unix':req.params.param,'natural':dateNatural});
+        }else{
+          res.send({'unix':null,'natural':null});
+        }
+      }else{
+        if(moment(req.params.param).isValid()){
+          var dateUnix = moment(req.params.param).unix();
+          res.send({'unix':dateUnix,'natural':req.params.param});
+        }else{
+          res.send({'unix':null,'natural':null});
+        }
+      }
 });
 
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+var port = 8000;
+app.listen(port, function () {
+  console.log('Example app listening on port %s!',port);
 })
