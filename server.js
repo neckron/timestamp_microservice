88d2179323d6aefc0a9cd8a7cb4197aa00ed5fb4
Francisco <neckron2@gmail.com>
2017-02-08 17:31:58 +0000

    working on it

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var moment = require('moment'); 
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  //id = req.body.id;
+  res.render('index.ejs');
 })
+
+app.get('/:param', function(req, res) {
+    var valid = moment(req.params.param).isValid();
+    if(valid){
+        res.send({'unix':req.params.param,'natural':moment(req.params.param).toDate()})
+    }else{
+        res.send({'unix':'null','natural':'null'})
+    }
+  
+});
+
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
